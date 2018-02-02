964d1c5c10b36ec7ef9399ae766ca86ba996f8dd
Ejike Onwe <ejikeonwe@Ejikes-MacBook-Pro.local>
2018-02-01 20:39:10 -0500

    fixed minor server issue

--- server.js
+++ server.js
@@ -64,7 +64,7 @@ var tables = [
   customerID: 6}
   ];
 
-app.get('/tables', function (req, res) {
+app.get('/api/tables', function (req, res) {
   // console.log('table data requested');
   // var response = "testing";
   res.json(tables);
